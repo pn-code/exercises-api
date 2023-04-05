8dc0c1d2ab7b2ff37c1054f13937e698398248c1
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-05 14:23:21 -0700

    Moving auth to navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import Link from "next/link";
+import { SignIn, SignOutButton, useUser } from "@clerk/clerk-react";
+import { SignInButton } from "@clerk/nextjs";
 
 const Navbar = () => {
+  const user = useUser();
+
   return (
-    <nav className="flex justify-between p-4 bg-indigo-900">
+    <nav className="flex justify-between border-b border-slate-400 bg-indigo-900 p-4">
       <header className="cursor-pointer">
         <h1>LOGO</h1>
       </header>
       <ul className="flex gap-8">
         <Link href="/">Link</Link>
         <Link href="/">Link</Link>
-        <Link href="/">Link</Link>
+        {!user.isSignedIn && <SignInButton />}
+        {!!user.isSignedIn && <SignOutButton />}
+        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
       </ul>
     </nav>
   );
