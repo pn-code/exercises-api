a6b0519c336a070d76c643860ccb6b8638c8db6c
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-05 20:25:30 -0700

    Created relation between exercise authorId & user

--- src/server/api/routers/exercises.ts
+++ src/server/api/routers/exercises.ts
@@ -1,9 +1,33 @@
+import { User } from "@clerk/nextjs/dist/api";
+import { clerkClient } from "@clerk/nextjs/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    fullName: `${user.firstName} ${user.lastName}`,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
+
 export const exercisesRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.exercises.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const exercises = await ctx.prisma.exercises.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: exercises.map((exercise) => exercise.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return exercises.map((exercise) => ({
+      exercise,
+      author: users.find((user) => user.id === exercise.authorId),
+    }));
   }),
 });
