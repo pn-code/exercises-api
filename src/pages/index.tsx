05570a1a4147d6182701f4b304fc59c6a13e4567
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-06 13:14:43 -0700

    Added image to home page & added styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
+import heroImg from "./../../public/assets/hero-img.png";
 
 const Home: NextPage = () => {
   return (
@@ -11,24 +13,27 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex h-screen justify-center">
+      <main className="pt-8 flex h-screen flex-col px-2 lg:items-center lg:pt-[12%]">
         {/* HERO */}
-        <section className="flex w-full flex-col items-center">
-          <header>
-            <h1 className="text-5xl font-bold">Exercise API</h1>
-          </header>
+        <section className="flex w-full flex-col items-center justify-center gap-8 lg:flex-row sm:gap-[10%]">
+          <section className="flex flex-col items-center gap-8">
+            <p className="max-w-xl text-center text-3xl font-semibold lg:text-4xl lg:font-bold">
+              Elevate your fitness game with our intuitive API for resistance
+              and cardiovascular exercises.
+            </p>
+            <Link
+              className=" w-[80%] rounded-lg bg-indigo-900 p-4 text-center text-lg font-semibold hover:bg-indigo-600"
+              href="/"
+            >
+              Get Started
+            </Link>
+          </section>
 
-          <p className="text-3xl font-semibold text-amber-400">
-            Elevate your fitness game with our intuitive API for resistance and
-            cardiovascular exercises.
-          </p>
-
-          <Link
-            className="rounded-lg bg-indigo-900 p-4 text-lg font-semibold hover:bg-indigo-600"
-            href="/"
-          >
-            Get Started
-          </Link>
+          <Image
+            className="w-full max-w-lg rounded-md mt-4"
+            src={heroImg}
+            alt="exercises-api logo"
+          />
         </section>
       </main>
     </>
