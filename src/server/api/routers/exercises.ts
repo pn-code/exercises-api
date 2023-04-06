e3a434e64ee2fd911a2346747fc31aa5f12ca684
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-05 20:54:29 -0700

    Handling type error & added TRPCError

--- src/server/api/routers/exercises.ts
+++ src/server/api/routers/exercises.ts
@@ -1,5 +1,6 @@
 import { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
@@ -25,9 +26,19 @@ export const exercisesRouter = createTRPCRouter({
       })
     ).map(filterUserForClient);
 
-    return exercises.map((exercise) => ({
-      exercise,
-      author: users.find((user) => user.id === exercise.authorId),
-    }));
+    return exercises.map((exercise) => {
+      const author = users.find((user) => user.id === exercise.authorId);
+
+      if (!author)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for exercise not found.",
+        });
+
+      return {
+        exercise,
+        author,
+      };
+    });
   }),
 });
