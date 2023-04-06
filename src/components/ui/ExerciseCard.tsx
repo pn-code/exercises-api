fea6da4a5893717138fb8e7eeca69453043f00c0
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-05 21:04:51 -0700

    Adding author to Exercise Card

--- src/components/ui/ExerciseCard.tsx
+++ src/components/ui/ExerciseCard.tsx
@@ -7,14 +7,25 @@ const ExerciseCard = (props: ExerciseWithAuthor) => {
   const { exercise, author } = props;
 
   return (
-    <article className="text-white" key={exercise.id}>
+    <article className="flex gap-4 px-2 text-white" key={exercise.id}>
       <img className="w-[120px]" src={exercise.image} alt={exercise.name} />
-      <h2>{exercise.name}</h2>
-      <h3>by {author.fullName}</h3>
-      <p>{exercise.description}</p>
-      <a className="underline" href={exercise.demo}>
-        See Demo
-      </a>
+      <section className="flex flex-col gap-2">
+        <h2 className="inline text-lg font-semibold">{exercise.name}</h2>
+        <p>{exercise.description}</p>
+        <a className="underline" href={exercise.demo}>
+          See Demo
+        </a>
+        <footer className="flex gap-1">
+          <img
+            className="inline h-5 w-5 rounded-full"
+            src={author.profileImageUrl}
+            alt="author profile image"
+          />
+          <span className="block text-sm text-amber-300">
+            {author.fullName}
+          </span>
+        </footer>
+      </section>
     </article>
   );
 };
