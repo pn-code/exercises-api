272cfdd3ed4f9286c13bcb7961f97757e45544c9
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-06 20:28:32 -0700

    Added input states and create exercise handler

--- src/components/ui/CreateExerciseWizard.tsx
+++ src/components/ui/CreateExerciseWizard.tsx
@@ -1,14 +1,30 @@
 import { useUser } from "@clerk/nextjs";
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import Image from "next/image";
+import { api } from "~/utils/api";
 
 const CreateExerciseWizard = () => {
   const { user } = useUser();
   if (!user) return null;
 
+  // INPUTS
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [type, setType] = useState("cardio");
+  const [primaryTarget, setPrimaryTarget] = useState("cardio");
+  const [image, setImage] = useState("");
+  const [demo, setDemo] = useState("");
+
+  // Create Function
+  const { mutate } = api.exercises.create.useMutation();
+
+  const handleExerciseCreation = (e) => {
+    mutate({ name, description, type, primaryTarget, image, demo });
+  };
+
   return (
-    <form className="absolute inset-0 flex h-screen flex-col justify-center gap-2 bg-slate-900 px-2 lg:items-center">
-      <section className="flex flex-col gap-2 pt-10 lg:pt-4">
+    <form className="absolute inset-0 flex h-screen flex-col gap-2 bg-slate-900 px-2 lg:items-center">
+      <section className="flex flex-col gap-2 pt-4 lg:pt-4">
         <section className="flex items-center gap-2">
           <Image
             className="rounded-full"
@@ -20,51 +36,80 @@ const CreateExerciseWizard = () => {
           <h4>{user.fullName}</h4>
         </section>
 
-        <label htmlFor="">Exercise Name</label>
-        <input type="text" placeholder="Exercise Name" />
+        <label htmlFor="name">Exercise Name</label>
+        <input
+          id="name"
+          type="text"
+          placeholder="Exercise Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-        <label htmlFor="">Description</label>
+        <label htmlFor="desc">Description</label>
         <textarea
-          name=""
-          id=""
+          name="desc"
+          id="desc"
           cols={30}
           rows={10}
           placeholder="Describe exercise..."
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         />
 
-        <label htmlFor="">Exercise Type</label>
-        <select name="" id="">
-          <option value="">Cardiovascular</option>
-          <option value="">Resistance</option>
+        <label htmlFor="type">Exercise Type</label>
+        <select
+          name="type"
+          id="type"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+        >
+          <option value="cardio">Cardiovascular</option>
+          <option value="resistance">Resistance</option>
         </select>
 
-        <label htmlFor="">Primary Target</label>
-        <select name="" id="">
-          <option value="">Cardio</option>
-          <option value="">Neck</option>
-          <option value="">Rear Deltoids</option>
-          <option value="">Anterior Deltoids</option>
-          <option value="">Lateral Deltoids</option>
-          <option value="">Lats</option>
-          <option value="">Upper Back</option>
-          <option value="">Lower Back</option>
-          <option value="">Glutes</option>
-          <option value="">Hamstrings</option>
-          <option value="">Calves</option>
-          <option value="">Tibs</option>
-          <option value="">Quadriceps</option>
-          <option value="">Hip Flexors</option>
-          <option value="">Abdominals</option>
-          <option value="">Chest</option>
+        <label htmlFor="primaryTarget">Primary Target</label>
+        <select
+          name="primaryTarget"
+          id="primaryTarget"
+          value={primaryTarget}
+          onChange={(e) => setPrimaryTarget(e.target.value)}
+        >
+          <option value="cardio">Cardio</option>
+          <option value="neck">Neck</option>
+          <option value="rear deltoids">Rear Deltoids</option>
+          <option value="anterior deltoids">Anterior Deltoids</option>
+          <option value="lateral deltoids">Lateral Deltoids</option>
+          <option value="lats">Lats</option>
+          <option value="upper back">Upper Back</option>
+          <option value="lower back">Lower Back</option>
+          <option value="glutes">Glutes</option>
+          <option value="hamstrings">Hamstrings</option>
+          <option value="calves">Calves</option>
+          <option value="tibialis anterior">Tibs</option>
+          <option value="quadriceps">Quadriceps</option>
+          <option value="hip flexors">Hip Flexors</option>
+          <option value="abdominals">Abdominals</option>
+          <option value="chest">Chest</option>
         </select>
 
-        <label htmlFor="">Image URL</label>
-        <input type="text" placeholder="Add image url" />
+        <label htmlFor="image">Image URL</label>
+        <input
+          id="image"
+          type="text"
+          placeholder="Add image url"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
 
         <label htmlFor="">Demo URL</label>
-        <input type="text" placeholder="Add demo url" />
+        <input
+          type="text"
+          placeholder="Add demo url"
+          value={demo}
+          onChange={(e) => setDemo(e.target.value)}
+        />
 
-        <button className="mt-2 rounded-md bg-indigo-900 p-4">Submit</button>
+        <button onClick={handleExerciseCreation} className="mt-2 rounded-md bg-indigo-900 p-4">Submit</button>
       </section>
     </form>
   );
