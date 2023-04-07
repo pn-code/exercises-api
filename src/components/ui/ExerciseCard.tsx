563f6dff0ae917647b31d680fc862b01bddff778
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-06 19:18:28 -0700

    Handled Image Aspect Ratio

--- src/components/ui/ExerciseCard.tsx
+++ src/components/ui/ExerciseCard.tsx
@@ -1,28 +1,33 @@
 import Image from "next/image";
 import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime"
-
-dayjs.extend(relativeTime)
-
+import relativeTime from "dayjs/plugin/relativeTime";
 type ExerciseWithAuthor = RouterOutputs["exercises"]["getAll"][number];
+dayjs.extend(relativeTime);
 
 const ExerciseCard = (props: ExerciseWithAuthor) => {
   const { exercise, author } = props;
 
   return (
     <article className="flex gap-4 px-2 text-white" key={exercise.id}>
-      <Image
-        className="w-[120px]"
-        src={exercise.image}
-        alt={exercise.name}
-        width={120}
-        height={120}
-      />
+      <div className="relative w-48">
+        <Image
+          src={exercise.image}
+          alt={exercise.name}
+          layout={"fill"}
+          objectFit="contain"
+        />
+      </div>
+
       <section className="flex flex-col gap-2">
         <h2 className="inline text-lg font-semibold">{exercise.name}</h2>
         <p>{exercise.description}</p>
-        <a className="underline" href={exercise.demo} rel="noreferrer" target="_blank">
+        <a
+          className="underline"
+          href={exercise.demo}
+          rel="noreferrer"
+          target="_blank"
+        >
           See Demo
         </a>
         <footer className="flex gap-1">
